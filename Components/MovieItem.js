acca2d51b660d6e1aaaa6b0dc611645997e7b4b2
PLsergent <papa.louis59@gmail.com>
2019-02-19 17:04:39 +0100

    init recovery

---
+++ Components/MovieItem.js
@@ -0,0 +1,83 @@
+import React from 'react'
+import { StyleSheet, View, Text, Image } from 'react-native'
+import { getImage } from '../API/TMDBApi'
+
+export default class MovieItem extends React.Component {
+
+  render() {
+    const movie = this.props.movie
+    return (
+      <View style={styles.main_container}>
+        <Image
+         style={styles.image}
+         source={{uri: getImage(movie.poster_path)}}
+        />
+        <View style={styles.content}>
+          <View style={styles.header}>
+            <Text style={styles.title_text} numberOfLines={2}>{movie.title}</Text>
+            <Text style={styles.rating}>{movie.vote_average}</Text>
+          </View>
+          <View style={styles.description}>
+            <Text style={styles.description_text} numberOfLines={5}>{movie.overview}</Text>
+          </View>
+          <View style={styles.date}>
+            <Text style={styles.date_text}>Release : {movie.release_date}</Text>
+          </View>
+        </View>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  main_container: {
+    flex: 1,
+    flexDirection: 'row',
+    height: 190,
+    backgroundColor: '#cceeff',
+    marginBottom: 5
+  },
+  image: {
+    width: 120,
+    height: 180,
+    margin: 5,
+    backgroundColor: 'gray'
+  },
+  content: {
+    flex: 1,
+    margin: 5
+  },
+    header: {
+      flex: 3,
+      flexDirection: 'row',
+      alignItems: 'center'
+    },
+      title_text: {
+        flex: 4,
+        fontSize: 20,
+        flexWrap: 'wrap',
+        fontWeight: 'bold'
+      },
+      rating: {
+        textAlign: 'right',
+        flex: 1,
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: 'grey'
+      },
+    description: {
+      flex: 6,
+      marginTop: 5
+    },
+      description_text: {
+        fontSize: 16,
+        fontStyle: 'italic'
+      },
+    date:{
+      flex: 1,
+      marginBottom: 3
+    },
+      date_text: {
+        textAlign: 'right'
+      }
+})
---
+++ Components/Search.js
@@ -0,0 +1,107 @@
+import React from "react"
+import { StyleSheet, View, TextInput, Button, FlatList, Text, ActivityIndicator } from "react-native"
+import MovieItem from './MovieItem'
+import { getMovies } from '../API/TMDBApi'
+
+export default class Search extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.searchedText = ""
+    this.page = 0
+    this.totalPages = 0
+    this.state = {
+      movies: [],
+      isLoading: false
+    }
+  }
+
+  _displayLoading() {
+    if (this.state.isLoading) {
+      return (
+        <View style={styles.loading_container}>
+            <ActivityIndicator size='large' />
+        </View>
+      )
+    }
+  }
+
+  _loadMovies() {
+    if (this.searchedText.length > 0){
+      this.setState({ isLoading: true })
+      getMovies(this.searchedText, this.page+1).then(data => {
+        this.page = data.page
+        this.totalPages = data.total_pages
+        this.setState({
+          movies: [ ...this.state.movies, ...data.results ],
+          isLoading: false
+        })
+      })
+    }
+  }
+
+  _searchTextInputChanged(text) {
+        this.searchedText = text
+  }
+
+  _searchMovies() {
+    this.page = 0
+    this.totalPages = 0
+    this.setState({
+      movies: [],
+    }, () => {
+      this._loadMovies()
+    })
+  }
+
+  _keyExtractor = (item) => item.id.toString();
+
+  render() {
+    return (
+      <View style={styles.main_container}>
+        <TextInput style={styles.textinput}
+          onSubmitEditing={() => this._searchMovies()}
+          placeholder='Movie title'
+          onChangeText={(text) => this._searchTextInputChanged(text)}
+        />
+        <Button title='Search' onPress={() => this._searchMovies()}/>
+        <FlatList
+            data={this.state.movies}
+            keyExtractor={this._keyExtractor}
+            renderItem={({item}) => <MovieItem movie={item} />}
+            onEndReachTreshold={0.5}
+            onEndReached={() => {
+              if (this.page < this.totalPages) {
+                this._loadMovies()
+              }
+            }}
+        />
+        {this._displayLoading()}
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  main_container: {
+    flex: 1,
+  },
+  textinput: {
+    marginLeft: 5,
+    marginRight: 5,
+    marginBottom: 5,
+    height: 50,
+    borderColor: '#000000',
+    borderWidth: 1,
+    paddingLeft: 5
+  },
+  loading_container: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 100,
+    bottom: 0,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+})
